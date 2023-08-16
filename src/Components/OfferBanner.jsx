8d613aff54220142e97c9ae19a49bbcfb8246870
DumeGrisoni <misanthrope20218@gmail.com>
2023-08-16 13:04:30 +0200

    testing height

--- src/Components/OfferBanner.jsx
+++ src/Components/OfferBanner.jsx
@@ -1,7 +1,7 @@
 const OfferBanner = () => {
   return (
     // OFFER BANNER
-    <section className="flex w- flex-col bg-primary h-[258px] items-center text-center px-1 lg:px-2 justify-center gap-[20px] lg:gap-[30px] rounded-md shadow-md shadow-grey-font text-secondary-var-1">
+    <section className="flex flex-col bg-primary h-[300px] lg:h-[258px] items-center text-center px-1 lg:px-2 justify-center gap-[20px] lg:gap-[30px] rounded-md shadow-md shadow-grey-font text-secondary-var-1">
       <h1 className="text-headline ">Offre Parrainage</h1>
       <h2 className="text-title">
         J&apos;accroche + vous offre une heure de cours particulier gratuit pour
