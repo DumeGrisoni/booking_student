a57c8f7ed40995af972a92ac54d769866101baee
DumeGrisoni <misanthrope20218@gmail.com>
2023-08-26 15:18:46 +0200

    add connexion

--- src/pages/Bookings.jsx
+++ src/pages/Bookings.jsx
@@ -1,45 +1,74 @@
+/* eslint-disable no-unused-vars */
 import Text from '../Components/Text.jsx';
 import Button from '../Components/Button.jsx';
 import BookingForm from '../Components/BookingForm.jsx';
 import Calendar from '../Components/Calendar/Calendar.tsx';
+import { useState, useEffect } from 'react';
+import { createClient } from '@supabase/supabase-js';
+import Login from '../Components/Login.jsx';
 
 const Bookings = () => {
-  return (
-    <div className="flex flex-col lg:items-start items-center gap-10 px-[2rem] lg:px-[8rem] my-7 lg:h-[115vh]">
-      <h1 className="text-headline text-primary font bold">Réservation</h1>
-      <div className="flex flex-col lg:flex-row mx-auto w-full gap-10 lg:gap-[100px]">
-        <div className="flex flex-col gap-7 lg:gap-10 flex-1">
-          <BookingForm />
-          <section className="flex flex-col items-center gap-7">
-            <h2 className="text-default font-bold text-primary">
-              Choisir une date
-            </h2>
-            <Calendar />
-            <Button>Confirmer</Button>
-          </section>
-        </div>
-        <div className="flex flex-col h-full gap-7 lg:gap-[100px] flex-2">
-          <Text>
-            <p>
-              L&apos;entreprise se réserve le droit de refuser un rendez-vous ou
-              de modifier unilatéralement les prix de son offre à tout moment,
-              notamment en cas d&apos;augmentation des coûts, étant entendu que,
-              en cas d&apos;augmentation des prix postérieure à
-              l&apos;acceptation de l&apos;offre, seul le prix fixé au jour de
-              cette acceptation sera applicable au client.
-            </p>
-          </Text>
-          <Text>
-            <p className="text-bold">
-              Toutes les cases de couleurs blanche ne sont pas disponibles car
-              déjà réserver ou bien ce créneau ne correspond pas avec le type de
-              cours choisi
-            </p>
-          </Text>
+  const [session, setSession] = useState(null);
+
+  const supabase = createClient(
+    import.meta.env.VITE_SUPABASE_URL,
+    import.meta.env.VITE_SUPABASE_ANON_KEY
+  );
+
+  useEffect(() => {
+    supabase.auth.getSession().then(({ data: { session } }) => {
+      setSession(session);
+    });
+
+    const {
+      data: { subscription },
+    } = supabase.auth.onAuthStateChange((_event, session) => {
+      setSession(session);
+    });
+
+    return () => subscription.unsubscribe();
+  }, []);
+
+  if (!session) {
+    return <Login />;
+  } else {
+    return (
+      <div className="flex flex-col lg:items-start items-center gap-10 px-[2rem] lg:px-[8rem] my-7">
+        <h1 className="text-headline text-primary font bold">Réservation</h1>
+        <div className="flex flex-col lg:flex-row mx-auto w-full gap-10 lg:gap-[100px]">
+          <div className="flex flex-col gap-7 lg:gap-10 flex-1">
+            <BookingForm />
+            <section className="flex flex-col items-center gap-7">
+              <h2 className="text-default font-bold text-primary">
+                Choisir une date
+              </h2>
+              <Calendar />
+              <Button>Confirmer</Button>
+            </section>
+          </div>
+          <div className="flex flex-col h-full gap-7 lg:gap-[100px] flex-2">
+            <Text>
+              <p>
+                L&apos;entreprise se réserve le droit de refuser un rendez-vous
+                ou de modifier unilatéralement les prix de son offre à tout
+                moment, notamment en cas d&apos;augmentation des coûts, étant
+                entendu que, en cas d&apos;augmentation des prix postérieure à
+                l&apos;acceptation de l&apos;offre, seul le prix fixé au jour de
+                cette acceptation sera applicable au client.
+              </p>
+            </Text>
+            <Text>
+              <p className="text-bold">
+                Toutes les cases de couleurs blanche ne sont pas disponibles car
+                déjà réserver ou bien ce créneau ne correspond pas avec le type
+                de cours choisi
+              </p>
+            </Text>
+          </div>
         </div>
       </div>
-    </div>
-  );
+    );
+  }
 };
 
 export default Bookings;
